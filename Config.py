4711f3993a35f06039d976ae975c6bdf8ccfe563
repoog <repoog@gmail.com>
2016-03-17 16:57:36 +0800

    update: adjust main functions details and add conditional judgment

--- Config.py
+++ Config.py
@@ -1,6 +1,15 @@
 """
-Configure of Github account
+Github account and access token config
 """
 USER_NAME = "repoog"
 PASSWORD = ""
 ACCESS_TOKEN = ""
+
+"""
+Code search level config
+"""
+SEARCH_LEVEL = 1
+"""
+Default key word for GitPrey
+"""
+KEY_WORD = ""
--- GitPrey.py
+++ GitPrey.py
@@ -10,11 +10,11 @@ import threading
 import cookielib
 import json
 import base64
-import time
 from Config import *
 
-MAX_PAGE_NUM = 100  # maximum page number of github searching
-MAX_RLT_NUM_PAGE = 10   # maximum results count of per page
+MAX_PAGE_NUM = [10, 20, 50, 70, 100]  # maximum page number of github project searching
+MAX_CODE_PAGE = 100     # maximum results of code searching
+MAX_RLT_PER_PAGE = 10   # maximum results count of per page
 HOST_NAME = "https://github.com/"
 
 project_queue = Queue.Queue(0)
@@ -39,7 +39,10 @@ class GitPrey(object):
     """
     def __init__(self, keyword):
         self.keyword = quote(keyword)
-        self.search_url = "https://github.com/search?o=desc&p={page}&q={keyword}&ref=searchresults&type=Code&utf8=%E2%9C%93"
+        self.search_url = "https://github.com/search?o=desc&p={page}&q={keyword}+in%3Afile%2Cpath&ref=searchresults&type=Code&utf8=%E2%9C%93"
+        self.user_info_url = "https://api.github.com/users/{username}"
+        self.filename_check_url = "https://github.com/{project}/search?p={page}&utf8=%E2%9C%93&q=filename%3A{filename}"
+        self.content_check_url = "https://github.com/{project}/search?p={page}&utf8=%E2%9C%93&q={content}%20in:file"
 
     def search_project(self):
         """
@@ -47,22 +50,18 @@ class GitPrey(object):
         Returns: return related projects queue
         """
         self.__auto_login_github(USER_NAME, PASSWORD)  # get cookie of logining for crawling projects
-        unique_project_list = []
-        for i in range(1, MAX_PAGE_NUM+1):
+        for i in range(1, MAX_PAGE_NUM[SEARCH_LEVEL]+1):
             code_url = self.search_url.format(page=i, keyword=self.keyword)
             page_html = self.__get_page_html(code_url)
             cur_par_html = BeautifulSoup(page_html, "html.parser")
             project_info = cur_par_html.select("p.title > a:nth-of-type(1)")
             project_count = len(project_info)
             for j in range(0, project_count):
-                if project_info[j].string not in unique_project_list:
-                    unique_project_list.append(project_info[j].string)
+                if project_info[j].string not in project_queue.queue:
+                    project_queue.put(project_info[j].string)
             # deal the last page results with quiting page crawler
-            if project_count < MAX_RLT_NUM_PAGE:
+            if project_count < MAX_RLT_PER_PAGE:
                 break
-        # put unique project information into project queue for multiple threads dealing
-        for x in range(0, len(unique_project_list)):
-            project_queue.put(unique_project_list[x])
 
     def sensitive_info_check(self, project_info):
         """
@@ -78,24 +77,22 @@ class GitPrey(object):
         Check sensitive filename items in particular project
         Returns: None
         """
-        filename_check_url = "https://github.com/{project}/search?p={page}&utf8=%E2%9C%93&q=filename%3A{filename}"
         item_list = self.__pattern_db_list("PATTERN_DB/path.db")
         for i in xrange(len(item_list)):
-            file_list = []
             info_dic = {}
-            for j in xrange(MAX_PAGE_NUM):
-                check_url = filename_check_url.format(project=project, page=j, filename=item_list[i])
+            for j in range(1, MAX_CODE_PAGE+1):
+                check_url = self.filename_check_url.format(project=project, page=j, filename=item_list[i])
                 page_html = self.__get_page_html(check_url)
                 cur_par_html = BeautifulSoup(page_html, "html.parser")
                 file_info = cur_par_html.select("p.title > a:nth-of-type(1)")
                 file_count = len(file_info)
                 for file in file_info:
-                    info_dic["name"] = file.get("title")
-                    info_dic["html_url"] = file.get("href")
-                    file_list.append(info_dic.copy())
-                self.__output_match_result(file_list)
+                    name = file.get("title")
+                    html_url = file.get("href")
+                    info_dic[name] = html_url
+                self.__output_match_result(info_dic)
                 # deal the last page results with quiting page crawler
-                if file_count < MAX_RLT_NUM_PAGE:
+                if file_count < MAX_RLT_PER_PAGE:
                     break
 
     def __file_content_check(self, project):
@@ -103,24 +100,22 @@ class GitPrey(object):
         Check sensitive content items in particular project
         Returns: None
         """
-        content_check_url = "https://github.com/{project}/search?p={page}&utf8=%E2%9C%93&q={content}%20in:file"
         item_list = self.__pattern_db_list("PATTERN_DB/content.db")
         for i in xrange(len(item_list)):
-            file_list = []
             info_dic = {}
-            for j in range(1, MAX_PAGE_NUM+1):
-                check_url = content_check_url.format(project=project, page=j, content=item_list[i])
+            for j in range(1, MAX_CODE_PAGE+1):
+                check_url = self.content_check_url.format(project=project, page=j, content=item_list[i])
                 page_html = self.__get_page_html(check_url)
                 cur_par_html = BeautifulSoup(page_html, "html.parser")
                 file_info = cur_par_html.select("p.title > a:nth-of-type(1)")
                 file_count = len(file_info)
                 for file in file_info:
-                    info_dic["name"] = file.get("title")
-                    info_dic["html_url"] = file.get("href")
-                    file_list.append(info_dic.copy())
-                self.__output_match_result(file_list, item=item_list[i])
+                    name = file.get("title")
+                    html_url = file.get("href")
+                    info_dic[name] = html_url
+                self.__output_match_result(info_dic, item=item_list[i])
                 # deal the last page results with quiting page crawler
-                if file_count < MAX_RLT_NUM_PAGE:
+                if file_count < MAX_RLT_PER_PAGE:
                     break
 
     def __pattern_db_list(self, file_path):
@@ -145,8 +140,7 @@ class GitPrey(object):
         Returns: None
         """
         user_name, project_name = project.split(r"/")
-        user_info_url = "https://api.github.com/users/{username}"
-        user_info_url = user_info_url.format(username=user_name)
+        user_info_url = self.user_info_url.format(username=user_name)
         user_page = self.__get_page_html(user_info_url, token=ACCESS_TOKEN)
         user_data = json.loads(user_page)
         print "[+_+]User Nickname: %s" % user_data["login"]
@@ -156,18 +150,18 @@ class GitPrey(object):
         print "[+_+]Project Name: %s" % project_name
         print "[+_+]Project Link: %s%s" % (HOST_NAME, project)
 
-    def __output_match_result(self, file_list, item=None):
+    def __output_match_result(self, file_dic, item=None):
         """
         Output matching results from particular json data
         Returns: None
         """
-        for i in range(0, len(file_list)):
+        for key in file_dic:
             if item:
                 print "[-]Sensitive Word: %s" % item
-                print "[-]Compromise File: %s" % file_list[i]["name"]
+                print "[-]Compromise File: %s" % key
             else:
-                print "[-]Sensitive File: %s" % file_list[i]["name"]
-            print "[-]Compromise URL: %s%s" % (HOST_NAME, file_list[i]["html_url"])
+                print "[-]Sensitive File: %s" % key
+            print "[-]Compromise URL: %s%s" % (HOST_NAME, file_dic[key])
 
     def __auto_login_github(self, username, password):
         """
@@ -186,12 +180,13 @@ class GitPrey(object):
         Get parse html page from requesting url
         Returns: parse html page
         """
+        url = url.strip()
         handler = urllib2.HTTPCookieProcessor(cookie)
         opener = urllib2.build_opener(handler)
         headers = {'User-Agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/48.0.2564.116 Safari/537.36"}
         if data:
             postdata = urllib.urlencode(data)
-            request = urllib2.Request(url.strip(), data=postdata, headers=headers)
+            request = urllib2.Request(url, data=postdata, headers=headers)
         else:
             request = urllib2.Request(url, headers=headers)
         if token:
@@ -199,8 +194,6 @@ class GitPrey(object):
             request.add_header("Authorization", "Basic %s" % base64string)
         try:
             page_html = opener.open(request).read()
-        except httplib.BadStatusLine, e:
-            print e.code
         except urllib2.URLError, e:
             print "Error code: %s\nError reason: %s" % (e.code, e.reason)
             exit()
@@ -225,18 +218,50 @@ class MultipleThread(threading.Thread):
         super(MultipleThread, self).__init__()
         self.target = target
 
+def is_keyword_valid(keyword):
+    """
+    Check keyword input/config is valid or invalid
+    Returns: False if invalid, True if valid
+    """
+    if keyword.strip() == "" or keyword is None:
+        return False
+    else:
+        return True
+def is_level_valid(level):
+    """
+    Check search level config is valid or invalid
+    Returns: False if invalid, True if valid
+    """
+    if isinstance(level, int) and level in range(1, 6):
+        return True
+    else:
+        return False
+
 if __name__ == "__main__":
     print GitPrey.__doc__
 
-    # search projects according to key words which user inputs
-    keywords = raw_input("[*] Please input keywords of company for searching: ")
-    _gitprey = GitPrey(keywords)
+    # get key word config or input key word
+    if not is_keyword_valid(KEY_WORD):
+        keyword = raw_input("[*_*]Please input keywords of company for searching: ")
+        if not is_keyword_valid(keyword):
+            print "[!_!]ERROR INFO: There is not any key word.Please try again."
+            exit()
+    else:
+        keyword = KEY_WORD
+
+    # get search level config
+    if not is_level_valid(SEARCH_LEVEL):
+        print "[!_!]ERROR INFO: search level config is invalid."
+        exit()
+
+    # search projects according to key word and searching level
+    _gitprey = GitPrey(keyword)
     _gitprey.search_project()
     if not project_queue.empty():
         project_queue_size = project_queue.qsize()
-        print "[!]PROJECT INFO: Found [%d] public projects relating to the key words." % project_queue_size
+        print "[^_^]PROJECT INFO: Found [%d] public projects relating to the key word." % project_queue_size
     else:
-        print "[!]END INFO: There is not any project relating to the key words."
+        print "[^_^]END INFO: There is not any project relating to the key word."
         exit()
 
     # scan all projects with pattern path and pattern content
